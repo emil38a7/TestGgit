d6adb2b967eee8938769c55d4fc4389bddd17dfd
emil38a7 <emilk@DESKTOP-I0SDNNG>
2018-05-16 17:01:14 +0200

    testGitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Http, HttpModule } from '@angular/http';
 
-
-import { AppComponent } from './app.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { QuestionComponent } from './components/question/question.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    QuestionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'Question', component: QuestionComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
